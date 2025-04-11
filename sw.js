009a494d422054e280026fa68781eff7a781994a
Marlon Grochoska <nx250tornado2@gmail.com>
2025-04-11 22:37:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-const CACHE_NAME = 'image-resizer-cache-v4';
+const CACHE_NAME = 'image-resizer-cache-v5';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/resize.html'
+    '/resize.html',
+    '/assets/icon-192x192.png',
+    '/assets/icon-512x512.png',
+    '/assets/jszip.min.js',
+    '/assets/FileSaver.min.js'
 ];
 
 self.addEventListener('install', event => {
@@ -16,5 +20,20 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
